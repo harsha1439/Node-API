3f12c08ee7dd73a6d9eb940b1dcb29d2d9f72b3d
harsha1439 <harshasivasai85@gmail.com>
2021-08-09 20:50:25 -0400

    database and server setup done

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require('express')
+const express = require('express')               //express 
+require('dotenv').config();                     //to getch env variables
+const bodyparser = require('body-parser')       //body-parser to parse incoming req
+const mongoose = require('mongoose')
+
+const app = express() //start the app
+const port = process.env.PORT || 2000 //listen at that port
+//MongoDb database connection
+mongoose.connect(process.env.DATABASE, {useNewUrlParser:true,useUnifiedTopology:true})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () =>{
+    console.log('Database connected')
+})
 
 
-const app = express()
-const port = 3000
-
+// server setup
 app.listen(port, () => {
     console.log(`Express server is running at ${port}`)
 })
